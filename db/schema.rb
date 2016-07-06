1ad86589b5ff1844b12bea085175858edd8380de
haanh <ngohaanh764@gmail.com>
2016-07-06 13:52:16 +0700

    Fix static page 3rd

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,4 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160702075423) do
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
 end
