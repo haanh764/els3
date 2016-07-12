4f3842108f2e8c266184ec1e770b2ba50fe71530
haanh <ngohaanh764@gmail.com>
2016-07-12 09:59:24 +0700

    User sign in with remember session!

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions,      only: [:new, :create, :destroy]
 
   root "static_pages#home"
   get "/signup" => "users#new"
+  get "/signin" => "sessions#new"
   get "/help" => "static_pages#help"
   get "/about" => "static_pages#about"
 end
